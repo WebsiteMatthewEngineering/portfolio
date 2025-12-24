eee84300d916cfc898b9eefc488535ec6e6a27a0
Matthew Adam <67502293+MatthewEngineering@users.noreply.github.com>
2025-12-24 02:02:49 -0500

    change to button for new-tabs

--- portfolio/components/button.tsx
+++ portfolio/components/button.tsx
@@ -2,11 +2,16 @@ interface ButtonProps {
   href: string;
   children: React.ReactNode;
   className?: string;
+  new_tab?: boolean;
 }
 
-export default function Button({ href, children, className = "" }: ButtonProps) {
+export default function Button({ href, children, className = "", new_tab = false }: ButtonProps) {
   return (
-    <a href={href} className={`bg-sky-200 text-black font-semibold py-2 px-4 rounded-lg hover:bg-sky-500 transition-colors duration-200 w-full md:w-fit ${className}`}>
+    <a 
+      href={href} 
+      className={`bg-sky-200 text-black font-semibold py-2 px-4 rounded-lg hover:bg-sky-500 transition-colors duration-200 w-full md:w-fit ${className}`}
+      {...(new_tab && { target: "_blank", rel: "noopener noreferrer" })}
+    >
       <div className="mx-auto flex flex-row items-center">
         <p>{children}</p>
       </div>
