f5ec965296d4ad76339ffd772153f6690b0e5fd2
Matthew Adam <67502293+MatthewEngineering@users.noreply.github.com>
2025-12-24 00:30:04 -0500

    Update header.tsx

--- portfolio/components/header.tsx
+++ portfolio/components/header.tsx
@@ -1,15 +1,13 @@
-import Switcher from "@/components/switcher";
+// import Switcher from "@/components/switcher";
 
 export default function Header() {
-    return ( <header className="fixed w-full h-16 flex justify-between items-center px-4 z-10 bg-background-secondary text-text text-xl top-0">
+    return ( <header className="sticky w-full h-16 flex justify-between items-center px-4 z-10 bg-white text-black text-xl top-0">
           <a className="font-bold text-2xl content-center" href="/">Matthew Adam</a>
           <nav className="hidden md:flex">
-            <a className="px-4 h-16 cursor-pointer content-center items-center flex hover:bg-background-hover" href="/">Home</a>
-            <a className="px-4 h-16 cursor-pointer content-center items-center flex hover:bg-background-hover" href="/">Projects</a>
-            <a className="px-4 h-16 cursor-pointer content-center items-center flex hover:bg-background-hover" href="/">Skills</a>
+            <a className="px-4 h-16 cursor-pointer content-center items-center flex hover:bg-gray-100" href="/">Home</a>
+            <a className="px-4 h-16 cursor-pointer content-center items-center flex hover:bg-gray-100" href="/">Projects</a>
+            <a className="px-4 h-16 cursor-pointer content-center items-center flex hover:bg-gray-100" href="/">Skills</a>
           </nav>
- 
-        <Switcher />
           
         </header>
         )}
